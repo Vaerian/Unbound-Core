5a3178cd85781221f43a8594fa7b9ca509646470
Vaerian <vaeriann@gmail.com>
2020-09-05 02:05:51 -0600

    Rename project to Unbound

--- src/main/java/com/vaerian/magic/Magic.java
+++
@@ -1,9 +0,0 @@
-package com.vaerian.magic;
-
-import net.fabricmc.api.ModInitializer;
-
-public class Magic implements ModInitializer {
-
-	@Override
-	public void onInitialize() { }
-}
---
+++ src/main/java/com/vaerian/unbound/Unbound.java
@@ -0,0 +1,11 @@
+package com.vaerian.unbound;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Unbound implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+		// CODECs?
+	}
+}
